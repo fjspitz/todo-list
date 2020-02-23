6b441e5ce143a1f91dac87e730b022a55370ae10
Fernando J. Spitz <fjspitz@gmail.com>
2020-02-23 00:50:16 -0300

    Commit inicial

---
+++ src/main/java/ar/com/fjs/todolist/TodoListApplication.java
@@ -0,0 +1,13 @@
+package ar.com.fjs.todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/com/fjs/todolist/TodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.fjs.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
